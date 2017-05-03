716a5f9f35ec8dd8b29d8c145d06c15e9563bf30
anthonytailer <aalmeida@inf.ufsm.br>
2017-05-03 00:20:37 -0300

    Reformulação site de 2017

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE HTML>
+<html lang="pt-br"><head>
+
+	<meta charset="UTF-8" />
+	<title>SAINF</title>
+	<link rel="shortcut icon" href="img/favicon.png" type="image/png"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.min.css">
+
+	<script src="js/countDown.js" type="text/javascript"></script>
+	<script src="js/jquery.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/jquery.waypoints.min.js" type="text/javascript"></script>
+	<script src="js/script.js" type="text/javascript"></script>
+
+
+</head><body>
+	<div id="custom-bootstrap-menu" class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header"><a id="menu-logo" href=""></a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse navbar-menubuilder">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#a-sainf">A SAINF</a>
+                </li>
+                <li><a href="#apoio">Apoio</a>
+                </li>
+                <li><a href="#speakers">Speakers</a>
+                </li>
+                <li><a href="#contato">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+	</div>
+
+	<div style="display:block;height:60px;"></div>
+	<div id="cabecalho" class="container animated"> <!-- CABECALHO-->
+		<img class="container" src="img/sainf_banner.png"/>
+	</div><!--
+	--><div id="animate-cronomer" class="container animated">
+	 <!-- count down-->
+            <div id="clockdiv">
+            	<p>Faltam até o evento</p>
+            	<div class="listDown">
+                	<span class="days"></span>
+                	<div class="smalltext">Dias</div>
+              	</div>
+              	<div class="listDown">
+                	<span class="hours"></span>
+                	<div class="smalltext">Horas</div>
+              	</div>
+              	<div class="listDown">
+                	<span class="minutes"></span>
+                	<div class="smalltext">Minutos</div>
+              	</div>
+              	<div class="listDown">
+               		<span class="seconds"></span>
+                	<div class="smalltext">Segundos</div>
+              	</div>
+            </div>
+        <div id="a-sainf" class="menu-height"></div>
+    </div><!--
+    --><div class="container-fluid bg-cinza"> <!-- A SAINF -->
+		<div class="divisor">
+			<div class="title-section-grey"><i class="fa fa-commenting-o" aria-hidden="true"></i> A SAINF</div>
+		</div>
+		<div id="animate-sainf" class="animated">
+			<p>Organizado pelos alunos dos cursos de Sistemas de Informação e Ciência da Computação, a Semana Acadêmica da Informática, que está em sua 7ª edição, é um evento que conta com palestras, workshops e maratona de programação, com todas suas atividades voltadas para a área da Tecnologia da Informação.  Este ano, o evento ocorrerá entre os dias 06 e 09 de junho.</p>
+			<p>Durante o evento, são abordados assuntos em alta na área, como segurança, desenvolvimento web, software livre, games, robótica, linguagens de programação, computação forense, etc. Para a compor a programação do evento, são convidados profissionais, empresários e professores atuantes na área de TI para oferecer palestras e workshops.</p>
+		</div>
+		<div id="apoio" class="menu-height"></div>
+	</div><!--   
+	--><div class="container-fluid"><!-- APOIO -->
+		<div class="divisor">
+			<div class="title-section"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> APOIO</div>
+		</div>
+		<div id="animate-apoiadores" class="box-pai animated">
+			<div class="box"><a href ="https://www.sap.com/brazil/index.html" TARGET="_blank"><img src="img/apoiadores/sap.jpg"></a></div>
+			<div class="box"><a href ="http://www.cantustange.com.br/" TARGET="_blank"><img src="img/apoiadores/zada.png"></a></div>
+			<div class="box"><a href ="http://animati.com.br/" TARGET="_blank"><img src="img/apoiadores/animati.png"></a></div>
+			<div class="box"><a href ="https://www.treinaweb.com.br/" TARGET="_blank"><img src="img/apoiadores/treina_web.png"></a></div>
+			<div class="box"><a href ="https://polvolouco.com.br/" TARGET="_blank"><img src="img/apoiadores/polvo_louco.jpg"></a></div>
+			<div class="box"><a href ="http://ufsm.br/dainf" TARGET="_blank"><img src="img/apoiadores/dainf.PNG"></a></div><!--
+			--><div class="box"><a href ="http://www.inf.ufsm.br" TARGET="_blank"><img src="img/apoiadores/informatica.PNG"></a></div><!--
+			--><div class="box"><a href ="http://www.ufsm.br" TARGET="_blank"><img src="img/apoiadores/ufsm.PNG"></a></div><!--
+			--><div id="speakers" class="menu-height"></div>
+		</div>
+	</div><!--
+	--><div id="speakersContainer" class="container-fluid speakers bg-cinza"> <!-- SPEAKERS -->
+		<div class="divisor">
+			<div class="title-section-grey"><i class="fa fa-comments-o" aria-hidden="true"></i> SPEAKERS</div>
+		</div>
+		<div id="animate-speakers" class="speakers text-center center-block animated">
+			<!-- Daniel - Sistemas de Recomendação -->
+			<div class="speaker-cima">
+				<img class="img-responsive img-circle" src="img/palestrantes/daniel.jpg">
+				<h5>Daniel Lichtnow</h5>
+				<h6>UFSM</h6>
+			</div>
+			<!-- Matheus - UFRGS -->
+			<div class="speaker-cima">
+				<img class="img-responsive img-circle" src="img/palestrantes/matheus_serpa.jpg">
+				<h5>Matheus Serpa</h5>
+				<h6>UFRGS | Intel</h6>
+			</div>
+		</div><!--
+			--><div id="contato" class="menu-height"></div>
+	</div><!--
+	--><div class="container-fluid contato"> <!-- CONTATO -->
+		<div class="divisor">
+			<div class="title-section"><i class="fa fa-comments-o" aria-hidden="true"></i> CONTATO</div>
+		</div>
+		<p><br/>Curta nossa página e participe do evento no Facebook e fique por dentro de todas as novidades!<br/>
+		<a class="link" href="http://fb.com/ufsmSAINF" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i> Facebook</a> |
+		<a class="link" href="https://www.facebook.com/events/441486116189600/" target="_blank"><i class="fa fa-calendar" aria-hidden="true"></i> Evento </a></p>		
+		<p>Para quaisquer outras dúvidas ou sugestões, entre em contato pelo e-mail:</p>
+		<p class="link"><i class="fa fa-envelope" aria-hidden="true"></i> sainf@inf.ufsm.br</p>
+	</div><!--
+	--><div  class="container-fluid rodape"> <!-- RODAPE -->
+		<div class="divisor"></div>
+		<p>Copyright &copy <?= Date("Y"); ?> SAINF<br/>Universidade Federal de Santa Maria<br/>RS - Brasil</p>
+	</div>
+<script src="js/countDown.js" type="text/javascript"></script>
+</body></html>			
--- js/script.js
+++ js/script.js
@@ -1,32 +1,63 @@
+$(document).ready(function(){
 
-var w, h, ms, body, html, menu;
+  var $cabecalho = $('#cabecalho');
+  var $cronometro = $('#animate-cronomer');
+  var $asainf = $('#animate-sainf');
+  var $apoiadores = $('#animate-apoiadores');
+  var $speakers = $('#animate-speakers');
+  var $cronograma = $('#animate-cronograma');
 
-function adjust(){
-	w=window.innerWidth; h=window.innerHeight;
+  
+  $cabecalho.css('opacity', '0');
+  $cronometro.css('opacity', '0');
+  $asainf.css('opacity', '0');
+  $apoiadores.css('opacity', '0');
+  $speakers.css('opacity', '0');
+ 
+  $cabecalho.waypoint(function(direction) {
+  	if (direction == 'down' ) {
+  		$cabecalho.addClass('fadeInDownBig bounceOut');	
+  	}else {
+  		$cabecalho.removeClass('fadeInDownBig bounceOut');	
+  	}
+     
+  }, { offset: '75%' });
 
-	//html.style.width=w+"px"; html.style.height=h+"px";
-	//body.style.width=w+"px"; body.style.height=h+"px";
-	menu.style.height="46px";
-	ms=false;
-}
+  $cronometro.waypoint(function(direction) {
+  	if (direction == 'down' ) {
+  		$cronometro.addClass('fadeInRight');	
+  	}else {
+  		$cronometro.removeClass('fadeInRight');	
+  	}
+     
+  }, { offset: '75%' });
 
-function ready(){
-	body=document.getElementsByTagName("body")[0];
-	html=document.getElementsByTagName("html")[0];
-	menu=document.getElementById("menu");
-	adjust();
-}
+  $asainf.waypoint(function(direction) {
+  	if (direction == 'down' ) {
+  		$asainf.addClass('fadeInLeft');	
+  	}else {
+  		$asainf.removeClass('fadeInLeft');	
+  	}
+     
+  }, { offset: '50%' });
 
-function back(){
-	menu.style.height="46px";
-	ms=false;
-}
+   $apoiadores.waypoint(function(direction) {
+  	if (direction == 'down' ) {
+  		$apoiadores.addClass('fadeInRight');	
+  	}else {
+  		$apoiadores.removeClass('fadeInRight');	
+  	}
+     
+  }, { offset: '50%' });
 
-function drop(){
-	if(ms==false){
-		menu.style.height="329px";
-		ms=true;
-	}
-	else back();
-}
+    $speakers.waypoint(function(direction) {
+  	if (direction == 'down' ) {
+  		$speakers.addClass('fadeInLeft');	
+  	}else {
+  		$speakers.removeClass('fadeInLeft');	
+  	}
+     
+  }, { offset: '50%' });
+
+});
 
