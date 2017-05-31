70a29434425dbf5f30b21ab694ea06ed8134b375
lucaslioli <lucaslima.o@outlook.com>
2017-05-30 23:21:00 -0300

    Atualização de informação

--- pages/maratona.php
+++ pages/maratona.php
@@ -6,7 +6,7 @@
 			<p>A Maratona de Programação da SAINF ocorrerá no <b>dia 07 de junho</b> (quarta-feira) <b>das 10:30 às 18:00</b> com pausa para almoço e <b>coffee break às 16:00</b>.</p>
 			<p>Podem se inscrever grupos de ATÉ 3 pessoas que tenham a inscrição confirmada no evento, SEM custo adicional. A logística será aos moldes da <a href="http://maratona.ime.usp.br/info17.html" target="_blank">Maratona de Programação da SBC</a>, sendo dividida em 2 níveis: <b>Iniciante</b> (alunos até o 4° semestre) e <b>Mediano</b> (alunos a partir do 5°). OBS: Alunos a partir do 5° semestre não podem participar do nível Iniciante, mas alunos até o 4° semestre podem participar do nível mediano.</p>
 			<p>A premiação oferecida será de <strong>R$ 150,00</strong> para a equipe vencedora de cada nível!</p>
-			<p>A maratona ocorrerá nos laboratórios do Centro de Tecnologia da UFSM, não sendo necessária a utilização de notebooks pessoais.
+			<p>A maratona ocorrerá nos laboratórios do Centro de Tecnologia da UFSM, não sendo necessária a utilização de notebooks pessoais. A linguagem de programação adotada para a resolução dos desafios será o C.</p>
 			<p><strong>Vagas limitadas!</strong> Faça a inscrição do seu grupo pelo formulário <strong>até as 12:30 de 06/06</strong> (primeiro dia do evento):</p>
 		</div>
 		<div class="text-center">
