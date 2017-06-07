64fb5c5f9aab911f6ce4c940fb2e0ae1f90903a4
lucaslioli <lucaslima.o@outlook.com>
2017-06-06 21:34:34 -0300

    Update menu

--- pages/menu.php
+++ pages/menu.php
@@ -9,12 +9,8 @@
                 <li><a href="#a-sainf">A SAINF</a></li>
                 <li><a href="#cronograma">Cronograma</a></li>
                 <li><a href="#speakers">Speakers</a></li>
-                <?php if($prazo_evento > $agora): ?>
-                    <li><a href="#inscricoes">Inscrições</a></li>
-                <?php endif; 
-                if($prazo_maratona > $agora):?>
-                    <li><a href="#maratona">Maratona</a></li>
-                <?php endif; ?>
+                <li><a href="#inscricoes">Inscrições</a></li>
+                <li><a href="#maratona">Maratona</a></li>
                 <li><a href="#apoio">Apoio</a></li>
                 <li><a href="#gallery">Galeria</a></li>
                 <li><a href="#contato">Contato</a></li>
